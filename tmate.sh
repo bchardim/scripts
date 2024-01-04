2f02991dcaeb901e3be3b3e208a5411ab7a6d506
Benjamin Chardi Marco <bchardim@redhat.com>
2024-01-04 11:51:12 +0100

    Add tmate.sh

---
+++ tmate.sh
@@ -0,0 +1,4 @@
+mkdir -p /tmp/tmate && cd /tmp/tmate
+wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz
+tar -xf tmate-2.4.0-static-linux-amd64.tar.xz
+./tmate-2.4.0-static-linux-amd64/tmate -F
