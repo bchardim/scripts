5f011bd0ab553041413c0ad458f68702aabfbf8a
Benjamin Chardi Marco <bchardim@redhat.com>
2024-01-15 14:14:51 +0100

    Add enable-cloudinit.sh

--- enable-cloudinit.sh
+++ enable-cloudinit.sh
@@ -1,7 +1,16 @@
 echo "network: {config: disabled}" >> /etc/cloud/cloud.cfg
-echo "network: {config: disabled}" >> /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
 echo "ssh_pwauth:   1" >> /etc/cloud/cloud.cfg
-for i in cloud-init cloud-config cloud-init-local cloud-final
-do
-   systemctl enable $i
-done 
+cloud-init clean --log
+systemctl enable cloud-init.service
+systemctl enable cloud-config.service
+systemctl enable cloud-init-local.service
+systemctl enable cloud-final.service
+rm -rf /tmp/callback.sh
+rm -rf /var/lib/cloud/instance
+rm -rf /var/lib/cloud/instances
+rm -rf /var/lib/cloud/data
+rm -rf /var/lib/cloud/sem/config_scripts_per_once.once
+rm -rf /var/log/cloud-init.log
+rm -rf /var/log/cloud-init-output.log
+echo $?
