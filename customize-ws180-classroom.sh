6b4d9352132565f417d06469cf725519c17f65b6
Benjamin Chardi Marco <bchardim@redhat.com>
2024-11-12 17:14:45 +0100

    Fix customize-ws180-classroom.sh

--- customize-ws180-classroom.sh
+++ customize-ws180-classroom.sh
@@ -1,2 +1,4 @@
-cp *.mp4 /content/courses/ws180/4.14/materials/video/
+mkdir -p /content/courses/rh92/rhel9.2/materials/video
+cp *.mp4 /content/courses/rh92/rhel9.2/materials/video
+wget http://materials.example.com/video/4-facility.mp4
 echo $?
