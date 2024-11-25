03f783ece54cf61f393388b22b40411ff353f187
Benjamin Chardi Marco <bchardim@redhat.com>
2024-11-25 10:01:03 +0100

    Fix install_local_package.sh

--- install_local_package.sh
+++ install_local_package.sh
@@ -1,3 +1,3 @@
-sed -i 's/SUCCESS=0/SUCCESS=0; PKG_ENV=prod/mg' /usr/local/sbin/dynolabs-update.sh
-systemctl restart dynolabs-update.service
-su - student -c 'source .venv/labs/bin/activate && pip list | grep rht && deactivate && echo $?'
+sudo sed -i 's/SUCCESS=0/SUCCESS=0; PKG_ENV=prod/mg' /usr/local/sbin/dynolabs-update.sh
+sudo systemctl restart dynolabs-update.service
+source .venv/labs/bin/activate && pip list | grep rht && deactivate && echo $?
