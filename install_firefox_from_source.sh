01aa85142bb8f2e38200646492f637f739c8027d
Benjamin Chardi Marco <bchardim@redhat.com>
2025-01-07 16:25:57 +0100

    Add version to install_firefox_from_source.sh

--- install_firefox_from_source.sh
+++ install_firefox_from_source.sh
@@ -1,9 +1,10 @@
 VER=133.0.3
 dnf remove -y firefox
+rm -rf /opt/firefox
+rm -rf /home/student/.cache/mozilla/firefox
 curl https://download-installer.cdn.mozilla.net/pub/firefox/releases/${VER}/linux-x86_64/en-US/firefox-${VER}.tar.bz2 -o firefox-${VER}.tar.bz2
 tar xjf firefox-${VER}.tar.bz2 
-rm -rf /opt/firefox
 mv firefox /opt
 ln -s /opt/firefox/firefox /usr/local/bin/firefox
 rm -rf firefox*
-#wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
