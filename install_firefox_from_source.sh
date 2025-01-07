6073eaf9383ca28d6da627196f8fd5804b5a5754
Benjamin Chardi Marco <bchardim@redhat.com>
2025-01-07 18:46:20 +0100

    Create student profile, test

--- install_firefox_from_source.sh
+++ install_firefox_from_source.sh
@@ -1,6 +1,7 @@
 VER=133.0.3
 dnf remove -y firefox
 rm -rf /opt/firefox
+rm -rf /home/student/.mozilla
 rm -rf /home/student/.cache/mozilla/firefox
 curl https://download-installer.cdn.mozilla.net/pub/firefox/releases/${VER}/linux-x86_64/en-US/firefox-${VER}.tar.bz2 -o firefox-${VER}.tar.bz2
 tar xjf firefox-${VER}.tar.bz2 
@@ -9,5 +10,3 @@ ln -s /opt/firefox/firefox /usr/local/bin/firefox
 sudo -u student sh -c "/usr/local/bin/firefox -CreateProfile student"
 rm -rf firefox*
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
-sed -i 's/Exec=firefox/Exec=firefox -P student/mg' /usr/local/share/applications/firefox.desktop
-echo $?
