c5007b7ca6a9b63cb82063d35c2e1f626546461e
Benjamin Chardi Marco <bchardim@redhat.com>
2025-01-08 19:17:39 +0100

    Configure firefox default homepage

--- install_firefox_from_source.sh
+++ install_firefox_from_source.sh
@@ -18,8 +18,9 @@ rm -rf firefox*
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
 
 # Configure firefox for student user
-echo "pkill firefox" | at now + 1 minutes
+echo "pkill firefox" | at now + 15 seconds
 sudo -u student sh -c "/usr/local/bin/firefox"
+sleep 30
 for pref in $(find /home/student/.mozilla/firefox -type f -name 'prefs.js')
 do
   echo "lockPref("browser.startup.homepage", "https://www.redhat.com/en");" >> ${pref}
