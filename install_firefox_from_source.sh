da9a280a06d683d879db3d9896cb4e71289f9d85
Benjamin Chardi Marco <bchardim@redhat.com>
2025-01-08 17:01:06 +0100

    Create student profile, done

--- install_firefox_from_source.sh
+++ install_firefox_from_source.sh
@@ -9,7 +9,5 @@ mv firefox /opt
 ln -s /opt/firefox/firefox /usr/local/bin/firefox
 rm -rf firefox*
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
-sleep 5
-echo "reboot" | at now + 20 seconds
 sudo -u student sh -c "/usr/local/bin/firefox"
 echo $?
